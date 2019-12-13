d27117549564961b2ce12737978a7bcd40e1f748
sam <crazysaem@gmail.com>
2019-12-13 11:05:44 +0100

    Switch from cra to parcel

--- src/index.tsx
+++ src/index.tsx
@@ -9,4 +9,15 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
+
+
+const main = async () => {
+    const overworld = await fetch('/Encounters_Overworld_Sword.txt');
+    const rare = await fetch('/Encounters_Rare_Sword.txt');
+
+    console.log(overworld);
+    console.log(rare);
+}
+
+main();
