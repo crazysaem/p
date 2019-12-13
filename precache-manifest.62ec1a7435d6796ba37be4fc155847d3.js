4b527583731bd1c404fd9a4647ea5fc13723cb57
sam <crazysaem@gmail.com>
2019-12-13 14:57:32 +0100

    Add page

---
+++ precache-manifest.62ec1a7435d6796ba37be4fc155847d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218c63c8c96ac856df74706cf83faab7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "575241934379f9cdf79d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6429c58f308ee570f898",
+    "url": "/static/js/2.b4589418.chunk.js"
+  },
+  {
+    "revision": "9652715b819d26dcc09aad3261a56a36",
+    "url": "/static/js/2.b4589418.chunk.js.LICENSE"
+  },
+  {
+    "revision": "575241934379f9cdf79d",
+    "url": "/static/js/main.77335076.chunk.js"
+  },
+  {
+    "revision": "5768baf0182c8dc3a3de",
+    "url": "/static/js/runtime-main.96f48b07.js"
+  }
+]);
